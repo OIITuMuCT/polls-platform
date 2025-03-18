8912d9104e87146b99da537a3ad714fb5912b3c2
OIITuMuCT <vataman@live.ru>
2025-03-18 21:16:44 +0300

    add settings VK

--- polls/settings.py
+++ polls/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "accounts",
     "store",
     "rest_framework",
+    "django_filters",
+    'social_django',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'polls.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -94,6 +96,12 @@ DATABASES = {
     }
 }
 
+AUTHENTICATION_BACKENDS = (
+    "social_core.backends.github.GithubOAuth2",
+    # "social_core.backends.vk.VKOAuth2",
+    "django.contrib.auth.backends.ModelBackend",
+)
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -128,9 +136,29 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ],
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+    ],
+}
+
+# github callback: http://example.com/complete/github/
+# example.com == your domain
+SOCIAL_AUTH_JSONFIELD_ENABLED = True
+# Auth GitHub
+SOCIAL_AUTH_GITHUB_KEY = "Ov23li8gKS4D919bNjnD"
+SOCIAL_AUTH_GITHUB_SECRET = "d98befb88c1767331d00c4fdef35b80b4ee756ba"
+# auth VK https://mysite.com/complete/vk-app
+# SOCIAL_AUTH_VK_OAUTH2_KEY = os.environ.get("VK_KEY")
+# SOCIAL_AUTH_VK_OAUTH2_SECRET = os.environ.get("VK_SECRET")
