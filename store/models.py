695a1074ef93d5dc0662942fe1fb36ddeefb05fa
OIITuMuCT <vataman@live.ru>
2025-03-19 18:33:05 +0300

    Добавлен owner

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Book(models.Model):
@@ -8,6 +9,7 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     author = models.CharField(max_length=255)
-
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    
     def __str__(self):
         return self.title
