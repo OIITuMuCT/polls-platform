68b010d08ddc363811b6e945adb3860235f1b4c2
OIITuMuCT <vataman@live.ru>
2025-03-19 11:01:38 +0300

    update store/models.py

--- store/models.py
+++ store/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Book(models.Model):
     """
     Класс книга 
-    attr: title, price """
+    attr: title, price, author """
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     author = models.CharField(max_length=255)
