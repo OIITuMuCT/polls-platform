af2e3c782d88b21c648f6764a35c4760c6597af7
OIITuMuCT <vataman@live.ru>
2025-03-19 11:26:38 +0300

    Тест: создание объекта Book

--- store/tests/test_api.py
+++ store/tests/test_api.py
@@ -1,10 +1,16 @@
+import json
 from django.urls import reverse
+from unittest import skip
 from rest_framework import status
 from rest_framework.test import APITestCase
 from store.models import Book
 from store.serializers import BookSerializer
+from django.contrib.auth.models import User
+
+
 class BooksApiTest(APITestCase):
     def setUp(self):
+        self.user = User.objects.create(username="test_username")
         self.book_1 = Book.objects.create(title='Test Book 1', price=25, author='Author 1')
         self.book_2 = Book.objects.create(title="Test Book 2", price=55, author='Author 2')
         self.book_3 = Book.objects.create(title="Test Book 3 Author 1", price=55, author='Author 1')
@@ -20,6 +26,7 @@ class BooksApiTest(APITestCase):
         self.assertEqual(serializer_data, response.data)
         print(response.data)
 
+    @skip
     def test_get_filter(self):
         """тест: получение filter"""
         url = reverse("book-list")
@@ -38,4 +45,21 @@ class BooksApiTest(APITestCase):
         serializer_data = BookSerializer([self.book_1, self.book_3], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
+        print(response.data)
+
+    def test_create(self):
+        """тест: создание объекта Book"""
+        url = reverse("book-list")
+        data = {
+            "title": "For Whom The Bell Tolls",
+            "price": 1000,
+            "author": "Ernest Hemingway",
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        print(url)
+        response = self.client.post(url, data=json_data, content_type="application/json")
+
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+
         print(response.data)
