352879940f8a3ff3ae0af5096469dc9aef616e87
OIITuMuCT <vataman@live.ru>
2025-03-19 20:50:01 +0300

    add readers in Book model

--- store/models.py
+++ store/models.py
@@ -9,7 +9,8 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     author = models.CharField(max_length=255)
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='my_books')
+    readers = models.ManyToManyField(User, through="UserBookRelation", related_name='books')
     
     def __str__(self):
         return self.title
@@ -27,3 +28,6 @@ class UserBookRelation(models.Model):
     like = models.BooleanField(default=False)
     in_bookmarks = models.BooleanField(default=False)
     rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES)
+
+    def __str__(self):
+        return f'{self.user.username}: "{self.book.title}", RATE: {self.rate}'
