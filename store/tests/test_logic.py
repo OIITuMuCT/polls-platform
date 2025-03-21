48be9fd39cd83f6bb33cb9fda059292e996acd20
OIITuMuCT <vataman@live.ru>
2025-03-21 18:54:33 +0300

    add test_logic

--- store/tests/test_logic.py
+++ store/tests/test_logic.py
@@ -1,18 +1,26 @@
-from store.logic import operations
+
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-class LogicTest(TestCase):
-    """ тест: проверка логики """
-    def test_plus(self):
-        """ тест сложение """
-        result = operations(7, 13, '+')
-        self.assertEqual(20, result)
+from store.models import Book, UserBookRelation
+from store.logic import set_rating
 
-    def test_minus(self):
-        """ тест разность """
-        result = operations(7, 13, '-')
-        self.assertEqual(-6, result)
-    def test_multiple(self):
-        """ тест произведение """
-        result = operations(5, 15, '*')
-        self.assertEqual(75, result)
+class SetRatingTest(TestCase):
+    """ тест: проверка рейтинга"""
+    def setUp(self):
+        user1 = User.objects.create(username='user1')
+        user2 = User.objects.create(username="user2")
+        user3 = User.objects.create(username='user3')
+
+        self.book_1 = Book.objects.create(title="Test Book 1", price=25,
+                                        author='Author 1', owner=user1)
+
+        UserBookRelation.objects.create(user=user1, book=self.book_1, like=True, rate=5)
+        UserBookRelation.objects.create(user=user2, book=self.book_1, like=True, rate=5)
+        UserBookRelation.objects.create(user=user3, book=self.book_1, like=True, rate=4)
+
+    def test_ok(self):
+        set_rating(self.book_1)
+        self.book_1.refresh_from_db()
+        self.assertEqual('4.67', str(self.book_1.rating))
+
