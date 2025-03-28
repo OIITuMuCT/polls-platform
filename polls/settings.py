3acb809ea12816b625e051d0f3d98591e2d6e6d6
OIITuMuCT <vataman@live.ru>
2025-03-28 19:07:20 +0300

    add questionnare app

--- polls/settings.py
+++ polls/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "django_filters",
     "social_django",
     "debug_toolbar",
+    'questionnaire',
 ]
 
 MIDDLEWARE = [
--- questionnaire/models.py
+++ questionnaire/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    """ Модель вопроса для анкеты """
+    QUESTION_LIST = {1: 'value 1', 2: 'value 2', 3: 'value 3', 4: 'value 4'}
+    title = models.CharField(max_length=250)
+    text = models.TextField()
+    answer_option = models.CharField(max_length=4, choices=QUESTION_LIST)
+
+
+class Polls(models.Model):
+    """Модель Опроса"""
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    question_list = models.ForeignKey(Question, on_delete=models.CASCADE)
